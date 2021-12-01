cf116b7aad66881db7aab25d87900c44a6d1cf44
Brad Leege <bleege@gmail.com>
2021-11-30 22:23:48 -0600

    Initial Commit

---
+++ MacMaps/MacMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMapsApp.swift
+//  MacMaps
+//
+//  Created by Brad Leege on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MacMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
